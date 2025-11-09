6f360add5f653146d681910a2a7f30522da274b7
NightLocke15 <christinefrances.holt@gmail.com>
2025-11-09 12:34:10 +0200

    Small fix

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -4,6 +4,7 @@ import { useFonts } from 'expo-font';
 import { Stack } from "expo-router";
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from "react";
+import { StatusBar } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
